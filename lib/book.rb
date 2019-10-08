2a69d86307fc65c26ea0444ab24bc6a9c67e65e6
Brianna Fetterley <bnfetterley@usfca.edu>
2019-10-08 19:58:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
 
   def initialize(title)
@@ -7,6 +7,10 @@ class Book
   end
 
   GENRES = []
+  
+  def genre=(genre)
+    
+  end
     
   def turn_page
     puts "Flipping the page...wow, you read fast!"
